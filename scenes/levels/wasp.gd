312a8616175fcd354ea5d26f79c489975a37e3fb
soggy <hello@eriktate.me>
2024-10-07 14:49:21 -0400

    saving wasp script

--- scenes/levels/wasp.gd
+++ scenes/levels/wasp.gd
@@ -1,13 +1,22 @@
-class_name Wasp
+# class_name Wasp
 
-extends Anchor
+# extends Anchor
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# @onready var los: RayCat2D = $los
+# var frog: Frog
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process() -> void:
-	pass
+# func find_frog() -> Frog:
+# 	var children := get_tree().get_root().get_children()
+# 	for child in children:
+# 		if child is Frog:
+# 			return child as Frog
+
+# # Called when the node enters the scene tree for the first time.
+# func _ready():
+# 	pass # Replace with function body.
+
+
+# # Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _physics_process(dt: float) -> void:
+# 	los.target_position = 
