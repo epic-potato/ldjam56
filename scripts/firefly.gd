707946b3b18d4e546c9d4829153d35651207aed7
soggy <hello@eriktate.me>
2024-10-06 21:48:57 -0400

    starting on mosquito

--- scripts/firefly.gd
+++ scripts/firefly.gd
@@ -29,6 +29,12 @@ func _ready():
 	pass # Replace with function body.
 
 
+func flutter(dt: float) -> void:
+	if sprite.position.distance_to(float_pos) > 0.1:
+		sprite.position = lerp(sprite.position, float_pos, dt * 3)
+	else:
+		float_pos = Vector2(randf() * 16 - 8, randf() * 16 - 8)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(dt: float) -> void:
 	if dead:
@@ -37,12 +43,7 @@ func _process(dt: float) -> void:
 			enable()
 		return
 
-	
-	if sprite.position.distance_to(float_pos) > 0.1:
-		sprite.position = lerp(sprite.position, float_pos, dt * 3)
-	else:
-		print("new float_pos")
-		float_pos = Vector2(randf() * 16 - 8, randf() * 16 - 8)
+	flutter(dt)
 
 
 func _on_hitbox_body_entered(body:Node2D):
